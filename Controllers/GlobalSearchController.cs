819a1940e704dc803869a4fdde3ef29df368f884
Max <maks2005khru@gmail.com>
2025-06-24 20:51:42 +0300

    added global search for singers, labels, albums and tracks

---
+++ Controllers/GlobalSearchController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VinylBack.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace VinylBack.Controllers
+{
+        [ApiController]
+        [Route("api/[controller]")]
+        public class GlobalSearchController : ControllerBase
+        {
+            private readonly VinylContext _context;
+
+            public GlobalSearchController(VinylContext context)
+            {
+                _context = context;
+            }
+
+        [HttpGet]
+        public async Task<IActionResult> Search([FromQuery] string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+                return BadRequest("Query cannot be empty.");
+
+            query = query.Trim().ToLower();
+            bool exactMatch = query.Length < 4;
+
+            var tracks = await _context.Track
+                .Where(t => exactMatch
+                    ? t.TrackName.ToLower() == query
+                    : t.TrackName.ToLower().Contains(query))
+                .Select(t => new
+                {
+                    Id = t.TrackId,
+                    Name = t.TrackName,
+                    Type = "Track"
+                })
+                .ToListAsync();
+
+            var singers = await _context.Singer
+                .Where(s => exactMatch
+                    ? s.SingerFullName.ToLower() == query
+                    : s.SingerFullName.ToLower().Contains(query))
+                .Select(s => new
+                {
+                    Id = s.SingerId,
+                    Name = s.SingerFullName,
+                    Type = "Singer"
+                })
+                .ToListAsync();
+
+            var labels = await _context.Lable
+                .Where(l => exactMatch
+                    ? l.LableName.ToLower() == query
+                    : l.LableName.ToLower().Contains(query))
+                .Select(l => new
+                {
+                    Id = l.LableId,
+                    Name = l.LableName,
+                    Type = "Label"
+                })
+                .ToListAsync();
+
+            var albums = await _context.Album
+                .Where(a => exactMatch
+                    ? a.AlbumName.ToLower() == query
+                    : a.AlbumName.ToLower().Contains(query))
+                .Select(a => new
+                {
+                    Id = a.AlbumId,
+                    Name = a.AlbumName,
+                    Type = "Album"
+                })
+                .ToListAsync();
+
+            var result = tracks
+                .Concat(singers)
+                .Concat(labels)
+                .Concat(albums);
+
+            return Ok(result);
+        }
+
+    }
+}
--- obj/Debug/net8.0/VinylBack.AssemblyInfo.cs
+++ obj/Debug/net8.0/VinylBack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VinylBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b9caccf4f514c32211da042252848e810ce5c3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9720a2e16ce83c1b5ac71d3d6f48d4fa7af9ed4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VinylBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VinylBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
